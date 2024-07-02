1b691daf3118c8394fd53e9fa679684e0464ee30
PadmanabhanM123 <padmanabhanm2003@gmail.com>
2024-07-02 16:59:55 +0530

    Added while.js

--- while.js
+++ while.js
@@ -1,6 +1,23 @@
+//while
 let i=5
 while(i<20)
     {
         console.log(i)
         i+=5
+    }
+
+//for in
+let car={
+    name:'bmw',colour:'red',model:'2023'
+}
+for(let i in car)
+    {
+        console.log(i+":"+car[i])
+    }
+
+//for of
+let car=['bmw','audi','bently']
+for(let i of car)
+    {
+        console.log(i)
     }
